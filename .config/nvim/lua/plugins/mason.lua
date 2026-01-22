47ef39c2ab598d21c61e269df22ca1d20079e5f0
Andrew <>
2026-01-21 18:44:42 -0800

    mason update

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -16,6 +16,22 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         automatic_enable = true,
+        ensure_installed = {
+          "basedpyright",
+          "cssls",
+          "dockerls",
+          "gh_actions_ls",
+          "graphql",
+          "harper_ls",
+          "jdtls",
+          "jsonls",
+          "lua_ls",
+          "ruff",
+          "rust_analyzer",
+          "sqlls",
+          "superhtml",
+          "ts_ls"
+        }
       })
     end,
   },
